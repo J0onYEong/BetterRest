ebfe51dd1b034a13c0feb79d24ddddd6d02c579a
choijun0 <cjy00050005@gmail.com>
2022-10-31 13:24:19 +0900

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by 최준영 on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
